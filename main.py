1c0a5cddc189afb65c33fd423b8a0a04ca773f64
sskskfskfj <dongkyun7717@gmail.com>
2026-04-06 16:52:07 +0900

    main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from similarity!")
+import sys
+from streamlit.web import cli as stcli
 
+def main():
+    # streamlit run app/front.py 명령어를 실행하는 것과 동일한 효과
+    sys.argv = ["streamlit", "run", "app/front.py"]
+    sys.exit(stcli.main())
 
 if __name__ == "__main__":
     main()
