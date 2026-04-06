d1d737f1dd43cb65f29cb7b75f6254ccf4178dd0
sskskfskfj <dongkyun7717@gmail.com>
2026-04-06 16:56:25 +0900

    add requirements.txt

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 from streamlit.web import cli as stcli
 
 def main():
-    # streamlit run app/front.py 명령어를 실행하는 것과 동일한 효과
     sys.argv = ["streamlit", "run", "app/front.py"]
     sys.exit(stcli.main())
 
